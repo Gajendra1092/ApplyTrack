9343d62eef353b6fd8d0a1993ebf43b9f1184a08
Gajendra <gajendranh1092@gmail.com>
2026-07-05 17:55:37 +0530

    Removed data and time tool, enhance form submission in extension UI

--- app.py
+++ app.py
@@ -34,34 +34,30 @@ model = ChatGoogleGenerativeAI(
     api_key=GOOGLE_API_KEY
 )
 
-def get_date_time() -> str:
-    """Get date and time."""
-    current = datetime.now().strftime("%d/%m/%Y %I:%M %p")
-    return str(current)
+# def get_date_time() -> str:
+#     """Get date and time."""
+#     current = datetime.now().strftime("%d/%m/%Y %I:%M %p")
+#     return str(current)
 
 agent = create_agent(
     model=model,
-    tools=[get_date_time],
+    # tools=[get_date_time],
     response_format=responseSchema,
     system_prompt="""
         You are an Helpful AI Assistant. You will take data as input and extract three things from the text:
             1. Company_name
             2. Role.
             3. Resume_name
-            4. Today's date and time.
                 
             Constraints:
                 1. If any of the required info is not present do not assume. Clearly tell that the info is not present or leave it empty.
-                2. For accessing data and time use tool get_date_time.
-                3. If the Resume_name if it starts with number ignore the number and take the give the resume name starting with english alphabet.
-            Tools:
-                1. get_date_time(): Tells the current date and time.
+                2. If the Resume_name if it starts with number ignore the number and take the give the resume name starting with english alphabet.
+
             Return only valid JSON with this exact structure:
                 {
                     "Company_name": "",
                     "Role": "",
                     "Resume_name": "",
-                    "Date_Time": ""
                 }
                 If any value is missing, use empty string.
             """
@@ -72,12 +68,9 @@ def call_agent(content: str):
     result = agent.invoke(
         {"messages": [{"role": "user", "content": content}]}
     )
-    print("Structured_response:", result["structured_response"])
+    
     structured = result["structured_response"]
     dict_response = structured.model_dump()
-
-    print(dict_response)
-    storeInSQLite(dict_response)
     return dict_response # dict
 
 # Store that dict in sql lite
@@ -93,7 +86,7 @@ def storeInSQLite(data):
             Date_Time TEXT
         )
     """)
-
+    
     cursor.execute("""
         INSERT INTO applications (
             Company_name,
@@ -112,29 +105,29 @@ def storeInSQLite(data):
 
     conn.commit() 
     conn.close()
-    with sqlite3.connect("applications.db") as conn:
-        cursor = conn.cursor()
-
-        cursor.execute("""
-            SELECT *
-            FROM applications
-            ORDER BY id DESC
-            LIMIT 1
-        """)
-
-    last_entry = cursor.fetchone()
-
-    print("Last Entry:",last_entry)
-    return last_entry
-
 
 @app.post("/handle-content")
 async def read_root(request: Request):
     text = await request.body()
     decoded_text = text.decode("utf-8")
+    data = call_agent(decoded_text) # dict
+    return data
 
-    extracted_fields = call_agent(decoded_text)
-    return {"result": extracted_fields}
+@app.post("/store-to-db")
+async def storingInDb(request: Request):
+    data = await request.body()
+    dict_data = json.loads(data)
+    DateTime = datetime.now().strftime("%d/%m/%Y %I:%M %p")
+    dict_data["Date_Time"] = str(DateTime)
 
+    try:
+        print("storing in db")
+        storeInSQLite(dict_data)
+        print("Data stored in DB",dict_data)
+    except Exception as err:
+        print(err)
+
+    return True
+    
     
     
